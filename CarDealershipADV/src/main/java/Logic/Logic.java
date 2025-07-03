89fe600699af3672aff6162aa54b554e129e3da9
krosengr4 <rosenkev4@gmail.com>
2025-07-03 06:51:27 -0500

    Added logic method for a user to sell a vehicle

--- CarDealershipADV/src/main/java/Logic/Logic.java
+++ CarDealershipADV/src/main/java/Logic/Logic.java
@@ -2,17 +2,25 @@ package Logic;
 
 import Data.LeaseContractDao;
 import Data.SalesContractDao;
+import Data.VehicleDao;
 import Data.mysql.MySqlLeaseDao;
 import Data.mysql.MySqlSalesDao;
+import Data.mysql.MySqlVehicleDao;
+import Models.SalesContract;
+import Models.Vehicle;
 import UI.UserInterface;
 import Utilities.Utils;
 import configurations.DatabaseConfig;
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Logic {
 
 	static SalesContractDao salesDao = new MySqlSalesDao(DatabaseConfig.setDataSource());
 	static LeaseContractDao leaseDao = new MySqlLeaseDao(DatabaseConfig.setDataSource());
+	static VehicleDao vehicleDao = new MySqlVehicleDao(DatabaseConfig.setDataSource());
 	static UserInterface ui = new UserInterface();
 
 	public static void processMainMenu() {
@@ -32,7 +40,39 @@ public class Logic {
 	}
 
 	private static void sellOrLeaseVehicle() {
-		System.out.println("Would you like to sell or lease?");
+		System.out.println("\n1 - Sell Vehicle\n2 - Lease Vehicle\n0 - Go Back");
+		int sellOrLease = Utils.getUserInputIntMinMax("Would you like to sell or lease?: ", 0, 2);
+
+		if(sellOrLease == 1)
+			sellVehicle();
+		else if(sellOrLease == 2)
+			leaseVehicle();
+	}
+
+	private static void sellVehicle() {
+		LocalDate date = LocalDate.now();
+		String customerName = Utils.getUserInput("Please enter customers first and last name: ");
+		String customerEmail = Utils.getUserInput("Pleas enter customers email: ");
+
+		int vehicleId = Utils.getUserInputInt("Enter the Vehicle ID that you are selling: ");
+		Vehicle vehicle = vehicleDao.searchById(vehicleId);
+
+		boolean isFinanced = false;
+		String stringIsFinanced = Utils.getUserInput("Is the vehicle financed? (Y or N): ").trim();
+
+		if(stringIsFinanced.equalsIgnoreCase("y"))
+			isFinanced = true;
+		else if (!stringIsFinanced.equalsIgnoreCase("n"))
+			System.err.println("ERROR! Enter either y or n!\nProcess cancelled.");
+
+		SalesContract contract = salesDao.add(new SalesContract(date, customerName, customerEmail, vehicle, isFinanced));
+
+		if(contract != null)
+			contract.print();
+	}
+
+	private static void leaseVehicle() {
+
 	}
 
 	private static void processGoToAdmin() {
