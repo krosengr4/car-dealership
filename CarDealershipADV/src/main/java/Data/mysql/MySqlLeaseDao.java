0ca4d96263a5b5eb7036ed7aa60fed6990b56651
krosengr4 <rosenkev4@gmail.com>
2025-07-03 04:37:38 -0500

    Created methods in Lease Dao for getting all lease contracts and mapping the result row as a lease contract

--- CarDealershipADV/src/main/java/Data/mysql/MySqlLeaseDao.java
+++ CarDealershipADV/src/main/java/Data/mysql/MySqlLeaseDao.java
@@ -1,4 +1,57 @@
 package Data.mysql;
 
-public class MySqlLeaseDao {
+import Data.LeaseContractDao;
+import Data.VehicleDao;
+import Models.Contract;
+import Models.LeaseContract;
+import Models.Vehicle;
+import configurations.DatabaseConfig;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySqlLeaseDao extends MySqlBaseDao implements LeaseContractDao {
+
+	static BasicDataSource dataSource = DatabaseConfig.setDataSource();
+	static VehicleDao vehicleDao = new MySqlVehicleDao(dataSource);
+
+	public MySqlLeaseDao(DataSource datasource) {
+		super(datasource);
+	}
+
+	@Override
+	public List<Contract> getAll() {
+		List<Contract> contractList = new ArrayList<>();
+		String query = "SELECT * FROM lease_contract;";
+
+		try(Connection connection = getConnection()) {
+			PreparedStatement statement = connection.prepareStatement(query);
+			ResultSet results = statement.executeQuery();
+
+			while(results.next()) {
+				LeaseContract contract = mapRow(results);
+				contractList.add(contract);
+			}
+
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+		return contractList;
+	}
+
+	private LeaseContract mapRow(ResultSet results) throws SQLException {
+		String date = results.getString("contract_date");
+		String customerName = results.getString("customer_name");
+		String customerEmail = results.getString("customer_email");
+		Vehicle vehicle = vehicleDao.searchById(results.getInt("vehicle_id"));
+
+		return new LeaseContract(date, customerName, customerEmail, vehicle);
+	}
 }
